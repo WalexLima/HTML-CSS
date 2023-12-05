fd290a577d466dd99c72a214aed26837154a7c08
WalexLima <151396969+WalexLima@users.noreply.github.com>
2023-12-05 20:35:51 -0300

    não finalizado!

--- 01 - Car/main.js
+++ 01 - Car/main.js
@@ -5,17 +5,17 @@ const features = document.querySelector('.feature-card-block')
 const constructionGrid =()=>{
     for (let cont =0; cont < 9; cont++) {
         const carro = document.createElement('div')
-        carro.classList.add('display-grid')
         carro.innerHTML += `
         <div class="card">
             <img src="img/car-card.png" alt="Car" />
             <div class="card-content">
-            <h3>Audi Supra TT</h3>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellendus.
-            </p>
-            <button type="button" class="buttonBlack">I want this!</button>
+                <h3>Audi Supra TT</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellendus.
+                </p>
+                <button type="button" class="buttonBlack">I want this!</button>
+            </div>
         </div>
         `
         filter.appendChild(carro)
